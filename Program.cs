2df0b11fe30ace94cbfff1c704045348c59f5367
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2025-09-14 19:43:25 -0700

    add needed packages. create database

--- Program.cs
+++ Program.cs
@@ -1,9 +1,36 @@
-﻿namespace docker_compose_example;
+﻿using Microsoft.Data.SqlClient;
+
+namespace docker_compose_example;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CreateDatabase();
+    }
+
+    private static void CreateDatabase()
+    {
+        const string connectionString = "Server=localhost,1433;User ID=sa;Password=YourStrong!Passw0rd;Encrypt=True;TrustServerCertificate=True";
+
+        try
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                const string sql = @"EXEC('CREATE DATABASE [NewDatabase]');";
+
+                using var cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();  
+
+                Console.WriteLine($"Database Created!.");
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine("Error occured");
+            Console.WriteLine(ex.Message);
+        }
+
     }
 }
